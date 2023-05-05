e2f1d07dbd43b77aeb7554a9f1b22c64dad0be12
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-05-05 19:33:19 +0530

    Added:StringPermutation problem

---
+++ StringPermutation.java
@@ -0,0 +1,41 @@
+//Java program to write a static function to return all permutation of string 
+		
+package com.java;
+import java.util.*;
+public class StringPermutation {
+	public static void main(String[] args) {
+		//Creating scanner class object and taking input from user
+		System.out.println("Enter a String :");
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		int n = str.length();
+        //creating StringPermutation class object and call permute method
+		StringPermutation permutation = new StringPermutation();
+		permutation.permute(str, 0, n - 1);
+		sc.close();
+
+	}
+
+	private void permute(String str, int l, int r) {
+		if (l == r)
+			System.out.println(str);
+		else {
+			for (int i = l; i <= r; i++) {
+				str = swap(str, l, i);
+				permute(str, l + 1, r);
+				str = swap(str, l, i);
+			}
+		}
+	}
+
+	public String swap(String a, int i, int j) {
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+}
+}
+
